a04b07394292f7b2dfae11c35d51502f671541da
n1ck_pro <59617443+N1ckPro@users.noreply.github.com>
2023-12-04 19:48:37 +0100

    chore: format

--- src/solutions/2023/04/part1.ts
+++ src/solutions/2023/04/part1.ts
@@ -2,19 +2,23 @@ import type { Solution } from '../../../lib/index.js';
 import { benchmark } from '../../../lib/index.js';
 
 export const solution: Solution<number> = (input: string[]) => {
-    let result = 0;
-    for (const line of input) {
-        const [winningCards, myCards] = line.slice(line.indexOf(':') + 2).trimStart().split(' | ').map(card => card.split(/ +/).map(Number));
+	let result = 0;
+	for (const line of input) {
+		const [winningCards, myCards] = line
+			.slice(line.indexOf(':') + 2)
+			.trimStart()
+			.split(' | ')
+			.map((card) => card.split(/ +/).map(Number));
 
-        let total = 0;
-        for (const card of myCards) {
-            if (winningCards.includes(card)) total++;
-        }
+		let total = 0;
+		for (const card of myCards) {
+			if (winningCards.includes(card)) total++;
+		}
 
-        if (total > 0) result += 2 ** (total - 1)
-    }
+		if (total > 0) result += 2 ** (total - 1);
+	}
 
-    return result;
-}
+	return result;
+};
 
 await benchmark(4, 1, solution);
