424ba4ef10d954280fa5d04b5a8c2fc0035587c6
n1ck_pro <59617443+N1ckPro@users.noreply.github.com>
2023-12-04 18:08:29 +0100

    ci: fix hardcoded end of line char

--- src/lib/input.ts
+++ src/lib/input.ts
@@ -1,7 +1,8 @@
 import { readFile } from 'node:fs/promises';
+import { EOL } from 'node:os';
 import { join } from 'node:path';
 
 export const readInput = async (day: number) => {
 	const input = await readFile(join('src', 'solutions', '2023', day >= 10 ? day.toString() : `0${day}`, 'input.txt'));
-	return input.toString().split(/\r\n/g);
+	return input.toString().split(EOL);
 };
