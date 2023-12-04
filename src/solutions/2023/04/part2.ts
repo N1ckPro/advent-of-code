375a89f06e5754d926e9496484f82d3f655a4ebe
n1ck_pro <59617443+N1ckPro@users.noreply.github.com>
2023-12-04 19:47:55 +0100

    Day 4 Solutions

---
+++ src/solutions/2023/04/part2.ts
@@ -0,0 +1,22 @@
+import type { Solution } from '../../../lib/index.js';
+import { benchmark } from '../../../lib/index.js';
+
+export const solution: Solution<number> = (input: string[]) => {
+    const cards: Map<number, number> = new Map([ ...Array.from({ length: input.length }).keys() ].map(i => [i, 1]))
+    for (const [i, line] of input.entries()) {
+        const [winningCards, myCards] = line.slice(line.indexOf(':') + 2).trimStart().split(' | ').map(card => card.split(/ +/).map(Number));
+
+        let total = 0;
+        for (const card of myCards) {
+            if (winningCards.includes(card)) total++;
+        }
+
+        for (let j = i + 1; j <= i + total; j++) {
+            if (cards.get(j)) cards.set(j, cards.get(j)! + cards.get(i)!)
+        }
+    }
+
+    return Array.from(cards.values()).reduce((a, b) => a + b);
+}
+
+await benchmark(4, 2, solution);
